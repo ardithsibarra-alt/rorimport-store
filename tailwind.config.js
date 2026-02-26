9e4b0f50d05519ca94a833a80f8c2de3cb21e73c
ardithsibarra-alt <ardithsibarra@gmail.com>
2026-02-26 10:48:42 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'navy': '#1e3a5f',
+        'gold': '#d4af37',
+      },
+    },
+  },
+  plugins: [],
+};
