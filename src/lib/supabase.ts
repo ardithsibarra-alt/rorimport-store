b41ffe2925d4c751750c0bcfb93aef936802d12f
ardithsibarra-alt <ardithsibarra@gmail.com>
2026-04-22 10:12:45 -0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Hemos reemplazado las variables VITE_ por los datos reales
+const supabaseUrl = 'https://lqpxqhsgiromaliybodv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxcHhxaHNnaXJvbWFsaXlib2R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MjI3MjgsImV4cCI6MjA5MjE5ODcyOH0.f7jUaJ8zPxYaDnrtpSMXGW_u1HKMwTP8yohF3T8ruYg';
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
