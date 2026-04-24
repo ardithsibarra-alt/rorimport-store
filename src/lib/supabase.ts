620b9151b60699a42a86745e78b05648ce378bb7
ardithsibarra-alt <ardithsibarra@gmail.com>
2026-04-24 09:53:16 -0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Hemos reemplazado las variables VITE_ por los datos reales
-const supabaseUrl = 'https://lqpxqhsgiromaliybodv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxcHhxaHNnaXJvbWFsaXlib2R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MjI3MjgsImV4cCI6MjA5MjE5ODcyOH0.f7jUaJ8zPxYaDnrtpSMXGW_u1HKMwTP8yohF3T8ruYg';
+// Usamos import.meta.env para que el sistema lea las variables de Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
