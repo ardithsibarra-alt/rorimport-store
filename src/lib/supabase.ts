dd8d19efe9a1cbc15dbb36b3d26490807173a51c
ardithsibarra-alt <ardithsibarra@gmail.com>
2026-04-24 10:33:42 -0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Esto hace que el código use las llaves que configuraste en Vercel
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
